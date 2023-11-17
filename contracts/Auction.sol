50fb34ef3fbba146512844a4e5c00abf1d1c2dfe
Lawrence Lim <ll4715@nyu.edu>
2023-11-17 17:55:11 -0500

    update
    9 tests passing

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,33 +2,34 @@
 pragma solidity ^0.8.13;
 
 contract Auction {
-
     address internal judgeAddress;
     address internal timerAddress;
     address internal sellerAddress;
     address internal winnerAddress;
-    uint winningPrice;
+    uint256 winningPrice;
 
     // TODO: place your code here
+    mapping(address => uint256) internal balances; // Balances to track funds
 
     // constructor
-    constructor(address _sellerAddress,
-                address _judgeAddress,
-                address _winnerAddress,
-                uint _winningPrice) payable {
-
+    constructor(
+        address _sellerAddress,
+        address _judgeAddress,
+        address _winnerAddress,
+        uint256 _winningPrice
+    ) payable {
         judgeAddress = _judgeAddress;
         sellerAddress = _sellerAddress;
-        if (sellerAddress == address(0))
-          sellerAddress = msg.sender;
+        if (sellerAddress == address(0)) sellerAddress = msg.sender;
         winnerAddress = _winnerAddress;
         winningPrice = _winningPrice;
+        balances[winnerAddress] = msg.value; // Assume the winner sends the bid amount
     }
 
     // This is used in testing.
     // You should use this instead of block.number directly.
     // You should not modify this function.
-    function time() public view returns (uint) {
+    function time() public view returns (uint256) {
         return block.number;
     }
 
@@ -36,24 +37,46 @@ contract Auction {
         return winnerAddress;
     }
 
-    function getWinningPrice() public view returns (uint price) {
+    function getWinningPrice() public view returns (uint256 price) {
         return winningPrice;
     }
 
     // If no judge is specified, anybody can call this.
     // If a judge is specified, then only the judge or winning bidder may call.
     function finalize() public virtual {
+        require(
+            this.auctionEnded(),
+            "Contract value is not equal to winning price."
+        );
+        require(
+            msg.sender == winnerAddress ||
+                (judgeAddress == address(0)) ||
+                (judgeAddress != address(0) &&
+                    (msg.sender == judgeAddress ||
+                        msg.sender == winnerAddress)),
+            "Unauthorized caller."
+        );
 
-        // TODO: place your code here
-
+        balances[sellerAddress] += winningPrice;
+        balances[winnerAddress] -= winningPrice;
     }
 
     // This can ONLY be called by seller or the judge (if a judge exists).
     // Money should only be refunded to the winner.
     function refund() public {
+        // if contract value is equal to winning price, then refund
+        require(
+            this.auctionEnded(),
+            "Contract value is not equal to winning price."
+        );
+        require(
+            msg.sender == sellerAddress ||
+                (judgeAddress != address(0) && msg.sender == judgeAddress),
+            "Unauthorized caller."
+        );
 
-        // TODO: place your code here
-
+        // Refund the winning bid to the winner
+        balances[winnerAddress] += winningPrice;
     }
 
     // Withdraw funds from the contract.
@@ -62,9 +85,16 @@ contract Auction {
     // Ensure that your withdrawal functionality is not vulnerable to
     // re-entrancy or unchecked-spend vulnerabilities.
     function withdraw() public {
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No funds to withdraw.");
 
-        //TODO: place your code here
-
+        balances[msg.sender] = 0; // Prevent re-entrancy
+        payable(msg.sender).transfer(amount);
     }
 
+    // check if the auction has ended
+    function auctionEnded() public view returns (bool) {
+        // Check if there exists a winner address
+        return winnerAddress != address(0);
+    }
 }
