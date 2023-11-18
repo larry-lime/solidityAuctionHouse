044a289259a6e8c255324cca10d974e95288d1b7
Lawrence Lim <ll4715@nyu.edu>
2023-11-17 19:17:03 -0500

    format

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -2,36 +2,54 @@
 pragma solidity ^0.8.13;
 
 import "./Auction.sol";
+import "hardhat/console.sol";
 
 contract DutchAuction is Auction {
-
-    uint public initialPrice;
-    uint public biddingPeriod;
-    uint public offerPriceDecrement;
+    uint256 public initialPrice;
+    uint256 public biddingPeriod;
+    uint256 public offerPriceDecrement;
 
     // TODO: place your code here
+    uint256 public startBlock;
 
     // constructor
-    constructor(address _sellerAddress,
-                          address _judgeAddress,
-                          uint _initialPrice,
-                          uint _biddingPeriod,
-                          uint _offerPriceDecrement)
-             Auction (_sellerAddress, _judgeAddress, address(0), 0) {
-
+    constructor(
+        address _sellerAddress,
+        address _judgeAddress,
+        uint256 _initialPrice,
+        uint256 _biddingPeriod,
+        uint256 _offerPriceDecrement
+    ) Auction(_sellerAddress, _judgeAddress, address(0), 0) {
         initialPrice = _initialPrice;
         biddingPeriod = _biddingPeriod;
         offerPriceDecrement = _offerPriceDecrement;
+        startBlock = time();
 
         // TODO: place your code here
-
     }
 
+    function getCurrentPrice() public view returns (uint256) {
+        uint256 blocksPassed = block.number - startBlock;
 
-    function bid() public payable{
+        // Ensure blocksPassed does not exceed biddingPeriod
+        if (blocksPassed > biddingPeriod) {
+            blocksPassed = biddingPeriod;
+        }
 
+        uint256 priceDecrease = blocksPassed * offerPriceDecrement;
+
+        // Ensure price does not go below reserve (minimum) price
+        if (initialPrice > priceDecrease) {
+            return initialPrice - priceDecrease;
+        } else {
+            return 0; // The minimum price reached
+        }
+    }
+
+    function bid() public payable {
         // TODO: place your code here
-
+        // require time() < biddingPeriod, "Bidding period has ended.";
+        require(time() <= startBlock + biddingPeriod, "Auction has ended");
+        require(msg.value >= getCurrentPrice(), "Bid is too low");
     }
-
 }
