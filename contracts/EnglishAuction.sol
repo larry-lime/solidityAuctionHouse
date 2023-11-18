700ca4a5504748bcd29e55618edcb5de32f370ac
Lawrence Lim <ll4715@nyu.edu>
2023-11-17 22:22:49 -0500

    passed tests

--- contracts/EnglishAuction.sol
+++ contracts/EnglishAuction.sol
@@ -4,39 +4,54 @@ pragma solidity ^0.8.13;
 import "./Auction.sol";
 
 contract EnglishAuction is Auction {
-
-    uint public initialPrice;
-    uint public biddingPeriod;
-    uint public minimumPriceIncrement;
-
-    // TODO: place your code here
+    uint256 public initialPrice;
+    uint256 public biddingPeriod;
+    uint256 public minimumPriceIncrement;
+    uint256 public lastBidTime;
+    address public highestBidder;
+    uint256 public highestBid;
 
     // constructor
-    constructor(address _sellerAddress,
-                          address _judgeAddress,
-                          uint _initialPrice,
-                          uint _biddingPeriod,
-                          uint _minimumPriceIncrement)
-             Auction (_sellerAddress, _judgeAddress, address(0), 0) {
-
+    constructor(
+        address _sellerAddress,
+        address _judgeAddress,
+        uint256 _initialPrice,
+        uint256 _biddingPeriod,
+        uint256 _minimumPriceIncrement
+    ) Auction(_sellerAddress, _judgeAddress, address(0), 0) {
         initialPrice = _initialPrice;
         biddingPeriod = _biddingPeriod;
         minimumPriceIncrement = _minimumPriceIncrement;
-
-        // TODO: place your code here
-
+        highestBid = initialPrice;
+        lastBidTime = block.number;
     }
 
-    function bid() public payable{
+    function bid() public payable {
+        require(
+            block.number <= lastBidTime + biddingPeriod,
+            "Bidding period has ended"
+        );
+        require(
+            msg.value >= highestBid + minimumPriceIncrement,
+            "Bid not high enough"
+        );
 
-        // TODO: place your code here
+        if (highestBidder != address(0)) {
+            // Refund the previous highest bidder
+            payable(highestBidder).transfer(highestBid);
+        }
 
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        lastBidTime = block.number; // Update the last bid time
     }
 
-    // Need to override the default implementation
-    function getWinner() public override view returns (address winner){
-
-        // TODO: place your code here
-
+    function getWinner() public view override returns (address winner) {
+        if (block.number > lastBidTime + biddingPeriod) {
+            return highestBidder;
+        }
+        return address(0); // No winner if the auction is still ongoing
     }
+
+    // Additional functions as needed, such as withdraw()
 }
